4ff4ac62709b8aa15ba7a7519794a02642413302
Eduardo Santos <dravirus21@gmail.com>
2022-05-25 14:16:48 -0300

    Code now works, adjusted variable types

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-function validarCPF(cpf) { 
-    if (cpf.lenght != 11) {
+function validarCPF(cpf) {
+    if (cpf.length != 11) {
         return false
     } else {
-        const numeros = cpf.substring(0, 9);
-        const digitos = cpf.substring(9);
+        let numeros = cpf.substring(0, 9);
+        let digitos = cpf.substring(9);
 
-        var soma = 0;
+        let soma = 0;
         for (var i = 10; i > 1; i--) {
-            soma = numeros.charAt(10 - i ) * i;
+            soma += numeros.charAt(10 - i) * i;
         }
+        console.log(soma);
 
         // variavel com pergunta direta de true or false, se for true dá 0, se for false dá a outra formula
-        var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+        let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
         //validação do primeiro digito
-        if (resultado != digitos.charAt(0))  {
+        if (resultado != digitos.charAt(0)) {
+            console.log("erro primeiro digito");
             return false;
         }
 
         soma = 0;
-
         numeros = cpf.substring(0, 10);
-        for (var k; k > 11; k--) {
+        
+        for (var k = 11 ; k > 1; k--) {
             soma += numeros.charAt(11 - k) * k;
         }
 
-        resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
         //validação do segundo digito
-        if (resultado != digitos.charAt(1)){
+        if (resultado != digitos.charAt(1)) {
+            console.log('erro segundo digito');
             return false;
         }
 
@@ -37,9 +40,9 @@ function validarCPF(cpf) {
 function validar() {
     document.getElementById('success').style.display = 'none';
     document.getElementById('error').style.display = 'none';
-    const cpf = document.getElementById('cpf_digitado').value;
+    let cpf = document.getElementById('cpf_digitado').value;
 
-    const resultadoValidacao = validarCPF(cpf);
+    let resultadoValidacao = validarCPF(cpf);
 
     if (resultadoValidacao) {
         document.getElementById('success').style.display = 'block';
